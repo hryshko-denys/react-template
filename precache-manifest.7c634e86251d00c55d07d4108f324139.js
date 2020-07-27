b25f47086b1999e2398e362d2fea394e447b1168
Denys <gryshko_denys@gmail.com>
2020-07-27 15:28:20 +0300

    Updates

---
+++ precache-manifest.7c634e86251d00c55d07d4108f324139.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed4fe607b78936996670b07c5533421e",
+    "url": "/react-template/index.html"
+  },
+  {
+    "revision": "fac8493d276408b11e7a",
+    "url": "/react-template/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b344a6cf2cb8c206fda0",
+    "url": "/react-template/static/js/2.7ab81765.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-template/static/js/2.7ab81765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fac8493d276408b11e7a",
+    "url": "/react-template/static/js/main.92d5d80a.chunk.js"
+  },
+  {
+    "revision": "22d71f0a45b553b62cb8",
+    "url": "/react-template/static/js/runtime-main.c9c040d8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-template/static/media/logo.5d5d9eef.svg"
+  }
+]);
